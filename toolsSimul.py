98c470930f6abf44c10411620828f718f1f23590
neonraged <neonraged@gmail.com>
2018-04-23 13:08:59 +0300

    Redone sim tools, now using vectors

--- toolsSimul.py
+++ toolsSimul.py
@@ -28,30 +28,67 @@ class Particle:
 
     #  Создание класса частиц -- единичных обьектов, за которые мы принимаем молекулы идеального газа
 
-    def __init__(self, types):
+    def __init__(self, r):
+
         self.x = int(input("X axis start point for particle:"))
         self.y = int(input("Y axis start point for particle:"))
         self.z = int(input("Z axis start point for particle:"))
-        self.v_x = int(input("X axis start velocity for particle:"))
-        self.v_y = int(input("Y axis start velocity for particle:"))
-        self.v_z = int(input("Z axis start velocity for particle:"))
-        self.ax = int(input("X axis start acceleration for particle:"))
-        self.ay = int(input("Y axis start acceleration for particle:"))
-        self.az = int(input("Z axis start acceleration for particle:"))
+        self.v = Vec(int(input()), int(input()), int(input()))
+        self.a = Vec(int(input()), int(input()), int(input()))
         self.dt = 0.1
+        self.r = r
         self.xmass = []
         self.ymass = []
         self.zmass = []
 
 
+class Vec:
+
+    def __init__(self, x, y, z):
+
+        self.coords = [x, y, z]
+
+    def __sub__(self, other):
+
+        if isinstance(other, Vec):
+
+            self.coords = [a - b for a, b in zip(self.coords, other.coords)]
+
+        else:
+
+            self.coords = [x - other for x in self.coords]
+
+    def __mul__(self, other):
+
+        if isinstance(other, Vec):
+
+            self.coords = [a * b for a, b in zip(self.coords, other.coords)]
+
+        else:
+
+            self.coords = [x*other for x in self.coords]
+
+    def __add__(self, other):
+
+        if isinstance(other, Vec):
+
+            self.coords = [a + b for a, b in zip(self.coords, other.coords)]
+
+        else:
+
+            self.coords = [x + other for x in self.coords]
+
+
 class AnimatedPlot:
 
     def __init__(self, envs):
+
             self.itercount = len(envs.particles)
             self.aniobj = []
             self.env = envs
 
     def addaniobj(self, axs):
+
         for i in range(self.itercount):
             obj = self.env.particles[i]
             aniobject, = axs.plot([obj.xmass[0]], [obj.ymass[0]], [obj.ymass[0]], marker='o', markersize=10)
@@ -61,6 +98,7 @@ class AnimatedPlot:
 class Gas:
 
     def __init__(self, partnum=1, types=0):
+
         self.partnum = partnum
         self.types = types
         self.particles = []
@@ -72,6 +110,7 @@ class Gas:
         self.max_z = int(input("Z axis max border:"))
 
     def addparticle(self):
+
         for i in range(self.partnum):
             self.particles.append(Particle(self.types))
 
@@ -93,37 +132,38 @@ class Gas:
 
     def bounce(self, p):
 
-        if p.x + p.v_x * p.dt + ((p.ax * (p.dt ** 2))/2) > self.max_x:
+        if p.x + p.v.coords[1] * p.dt + ((p.a.coords[1] * (p.dt ** 2))/2) > self.max_x:
             p.x = self.max_x
-            p.v_x = -p.v_x
+            p.v.coords[1] = -p.v.coords[1]
 
-        elif p.x + p.v_x * p.dt + ((p.ax * (p.dt ** 2))/2) < self.min_x:
+        elif p.x + p.v.coords[1] * p.dt + ((p.a.coords[1] * (p.dt ** 2))/2) < self.min_x:
             p.x = self.min_x
-            p.v_x = -p.v_x
+            p.v.coords[1] = -p.v.coords[1]
 
-        if p.y + p.v_y * p.dt + ((p.ay * (p.dt ** 2))/2) > self.max_y:
+        if p.y + p.v.coords[2] * p.dt + ((p.a.coords[2] * (p.dt ** 2))/2) > self.max_y:
             p.y = self.max_y
-            p.v_y = -p.v_y
+            p.v.coords[2] = -p.v.coords[2]
 
-        elif p.y + p.v_y * p.dt + ((p.ay * (p.dt ** 2))/2) < self.min_y:
+        elif p.y + p.v.coords[2] * p.dt + ((p.a.coords[2] * (p.dt ** 2))/2) < self.min_y:
             p.y = self.min_y
-            p.v_y = -p.v_y
+            p.v.coords[2] = -p.v.coords[2]
 
-        if p.z + p.v_z * p.dt + ((p.az * (p.dt ** 2))/2) > self.max_z:
+        if p.z + p.v.coords[3] * p.dt + ((p.a.coords[3] * (p.dt ** 2))/2) > self.max_z:
             p.z = self.max_z
-            p.v_z = -p.v_z
+            p.v.coords[3] = -p.v.coords[3]
 
-        elif p.z + p.v_z * p.dt + ((p.az * (p.dt ** 2))/2) < self.min_z:
+        elif p.z + p.v.coords[3] * p.dt + ((p.a.coords[3] * (p.dt ** 2))/2) < self.min_z:
             p.z = self.min_z
-            p.v_z = -p.v_z
+            p.v.coords[3] = -p.v.coords[3]
 
     def move(self, p):
-        p.x += p.v_x * p.dt + (p.ax * (p.dt ** 2)) / 2
-        p.y += p.v_y * p.dt + (p.ay * (p.dt ** 2)) / 2
-        p.z += p.v_z * p.dt + (p.az * (p.dt ** 2)) / 2
-        p.v_x += p.ax * p.dt
-        p.v_y += p.ay * p.dt
-        p.v_z += p.az * p.dt
-        p.particle.append(p.x)
+
+        p.x += p.v.coords[1] * p.dt + (p.a.coords[1] * (p.dt ** 2)) / 2
+        p.y += p.v.coords[2] * p.dt + (p.a.coords[1] * (p.dt ** 2)) / 2
+        p.z += p.v.coords[3] * p.dt + (p.a.coords[1] * (p.dt ** 2)) / 2
+        p.v.coords[1] += p.a.coords[1] * p.dt
+        p.v.coords[2] += p.a.coords[2] * p.dt
+        p.v.coords[3] += p.a.coords[3] * p.dt
+        p.xmass.append(p.x)
         p.ymass.append(p.y)
         p.zmass.append(p.z)
